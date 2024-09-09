5c8e86ce34a57c7ac204038f90471954b4cd12f8
Dai Pham V.[2] VN.Hanoi <dai.pham2@monstar-lab.com>
2024-09-09 14:59:54 +0700

    [singleton_pattern] Add new way to create singleton class

--- singleton_pattern/lib/singleton_pattern.dart
+++ singleton_pattern/lib/singleton_pattern.dart
@@ -1,19 +1,21 @@
 library singleton_pattern;
 
-export 'src/non_singleton.dart';
-export 'src/singleton.dart';
+import 'src/non_singleton.dart';
+import 'src/singleton.dart';
 
-// import 'src/non_singleton.dart';
-// import 'src/singleton.dart';
-//
-// void main() {
-//   final singleton = Singleton();
-//   final singleton2 = Singleton();
-//   print(
-//       "singleton & singleton2 have the same instance? ${identical(singleton, singleton2)}");
-//
-//   final nonSingleton = NonSingleton();
-//   final nonSingleton2 = NonSingleton();
-//   print(
-//       "nonSingleton & nonSingleton2 have the same instance? ${identical(nonSingleton, nonSingleton2)}");
-// }
+void main() {
+  final singleton = Singleton();
+  final singleton2 = Singleton();
+  print(
+      "singleton & singleton2 have the same instance? ${identical(singleton, singleton2)}");
+
+  final singletonAnother = Singleton2.instance;
+  final singletonAnother2 = Singleton2.instance;
+  print(
+      "singletonAnother & singletonAnother2 have the same instance? ${identical(singletonAnother, singletonAnother2)}");
+
+  final nonSingleton = NonSingleton();
+  final nonSingleton2 = NonSingleton();
+  print(
+      "nonSingleton & nonSingleton2 have the same instance? ${identical(nonSingleton, nonSingleton2)}");
+}
--- singleton_pattern/lib/src/singleton.dart
+++ singleton_pattern/lib/src/singleton.dart
@@ -16,3 +16,14 @@ class Singleton {
     print("Hello from Singleton!");
   }
 }
+
+// This is another approach to create a singleton class. And it is shorter than above.
+class Singleton2 {
+  // The static final keyword ensures that the class instance is created once and only once,
+  // when it’s accessed for the first time. After that, the same instance is returned each time.
+  static final Singleton2 instance = Singleton2._privateConstructor();
+
+  // The private constructor prevents external code from creating instances of the class directly.
+  // This ensures that the class can only be instantiated within itself.
+  Singleton2._privateConstructor();
+}
